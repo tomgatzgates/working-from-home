d68ef62543702d7ce9f7f5bf5221abc1304950a0
Tom Gates <tom@pactcoffee.com>
2017-08-21 20:47:25 +0100

    set port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 8080));
 
 app.get('/', function (request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port', app.get('port'));
+});
