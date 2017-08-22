b83caef766bab495b365a54c33b8ddd97a5ef5f8
Tom Gates <tom@pactcoffee.com>
2017-08-22 11:32:50 +0100

    slack 'add' command

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-const SlackRequest = function(data) {
-  const {action, ...text} = data.text;
+var SlackRequest = function(data) {
+  var splitText = data.text.split(' ');
+  var action = splitText[0];
+  var text = splitText.join(' ');
+
   this.action = action;
   this.text = text;
+  this.data = data;
 };
 
 SlackRequest.prototype.response = function() {
-  let response_type = 'ephemeral';
-  let text;
-  let attachments = [];
+  var responseType = 'ephemeral';
+  var text;
+  var attachments = [];
 
   switch(this.action) {
     case 'add':
       text = "Great, I'll let your team know";
+      break;
     default:
       text = "Uh oh, something went wrong";
+      break;
   }
 
-  return { response_type, text, attachments };
+  return { response_type: responseType, text: text, attachments: attachments };
 };
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 8080));
 app.post('/', function(request, response) {
+  console.log(request.body);
   const slackResponse = new SlackRequest(request.body).response();
+
   response.send(slackResponse);
 });
 
